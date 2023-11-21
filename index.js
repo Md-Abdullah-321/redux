86f07a122a35f9202d6d0a97af790fbe3c3ca0e3
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-11-21 12:22:55 +0600

    created reducer function

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 //definig constants
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
-const ADD_USER = "ADD_USER";
 
 //state
 const initialCounterState = {
     count: 0,
 }
 
-const initialUsersState = {
-    users: [{name: 'Md Abdullah'}]
-}
 
 
 //action - an Object that has type & payload
@@ -24,10 +20,23 @@ const decrementCounter = () => {
         type: DECREMENT,
     }
 }
-const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload: {name: 'Ashraful Islam'}
+
+
+//create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1,
+            };
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1,
+            };
+        default:
+            state;
     }
 }
 
